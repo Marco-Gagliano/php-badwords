bf5ce54c015711448415a70a82807689dec2c6d8
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-06 15:06:03 +0200

    preparazione-pagina

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<?php 
+
+/* 
+Descrizione:
+1. Creare una variabile con un paragrafo di testo a vostra scelta.
+2. Stampare a schermo il paragrafo e la sua lunghezza.
+3. Una parola da censurare viene passata dall’utente tramite parametro GET.
+4. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+*/
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP - Bad Words</title>
+
+    
+</head>
+<body>
+  
+</body>
+</html>
